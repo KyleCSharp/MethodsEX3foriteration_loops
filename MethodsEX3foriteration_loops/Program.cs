a8696d43c5afe9c1e4747998725a2272c1d9ab9a
KyleCSharp <kylelrigler@gmail.com>
2022-08-29 18:20:16 -0400

    Add project files.

---
+++ MethodsEX3foriteration_loops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodsEX3foriteration_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
